158a22fdb3f351b47ba7cd4b4879e446fa2fa650
Colin Li <213003@NA200-A001465.local>
2019-09-11 14:29:04 -0500

    start

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 3333
+
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => res.render('index.html'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
