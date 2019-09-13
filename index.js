60e39dc71c388b6b44caaa60698ffb205084396e
Colin Li <213003@NA200-A001465.local>
2019-09-13 15:14:48 -0500

    hi

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index.html');
-  res.render('scrabblesort.js');
+  res.render('scrabbleSort.js');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- public/ScrabbleSort.js
+++ public/ScrabbleSort.js
@@ -3,9 +3,14 @@ function pickLetters(){
   let letters = [];
   for(let i = 0; i < 8; i++){
     letters.push(String.fromCharCode((Math.floor(Math.random() * (26 - 1)) + 1) + 65));
-    document.getElementById("letterDisplay" + i).innerHTML = letters[i];
   }
   checkWords();
+/*  if(checkWords()){
+    for(let i = 0; i < 8; i++){
+      document.getElementById("letterDisplay" + i).innerHTML = letters[i];
+    }
+  }*/
+
 }
   function submitAnswer(){
     let answer = document.getElementById("answer").value;
@@ -14,5 +19,7 @@ function pickLetters(){
     pickLetters();
   }
   function checkWords(){
+	   var mydata = JSON.parse(dictionary);
+	    alert(mydata[0].a);
 
   }
