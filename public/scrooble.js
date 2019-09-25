760154eb216b15d6aee129922c8569399babdcf8
Colin8tor <colin8tor1206@gmail.com>
2019-09-25 17:24:17 -0500

    servre stuffs

--- public/scrooble.js
+++ public/scrooble.js
@@ -16,7 +16,7 @@ var scoreBoxG = 0;
 var scoreBoxB = 0;
 var highScore;
 
-let socket = io.connect(/*'https://scrooble.herokuapp.com/' || 'https://www.scrooble.net/' ||*/ 'localhost:3333');
+let socket = io.connect('https://scrooble.herokuapp.com/'/* || 'https://www.scrooble.net/' ||/* 'localhost:3333' */);
 socket.on('highscore', function(coolerScore) {
     highScore = coolerScore;
     document.getElementById("highScoreBoard").innerHTML = "Highscore: " + highScore;
